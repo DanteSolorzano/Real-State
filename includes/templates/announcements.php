f432005dfc4349182b2d6e9993e83f245951666f
DanteSolorzano <solorzanodante19@gmail.com>
2025-12-03 14:56:05 -0600

    (feature: Fix styles): fix a style bug on the cards

--- includes/templates/announcements.php
+++ includes/templates/announcements.php
@@ -12,45 +12,46 @@ $result = mysqli_query($db, $query);
 
 ?>
 
-<div class="announcements-container">
-    <?php while($propertie = mysqli_fetch_assoc($result)):  ?>
-        <div class="announcement">
-                
-            <img loading="lazy" src="/images/<?php echo $propertie['image']; ?>" alt="announcement">
+            <div class="announcements-container">
+                <?php while($propertie = mysqli_fetch_assoc($result)):  ?>
+                    <div class="announcement">
+                            
+                        <img class="img-propertie" loading="lazy" src="/images/<?php echo $propertie['image']; ?>" alt="announcement">
 
-            <div class="announcement-content">
+                        <div class="announcement-content">
                 
                 <h3>
                     <?php echo $propertie['title']; ?>
                 </h3>
 
                 <p>
-                   <?php echo $propertie['description']; ?>
+                <?php echo $propertie['description']; ?>
                 </p>
                 
-                <p class="price">$<?php echo $propertie['price']; ?></p>
+                <div class="price-features-container">
+                    <p class="price">$<?php echo $propertie['price']; ?></p>
 
-                <ul class="feature-icons">    
-                    <li>
-                        <img class="dark-icon" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="parking icon">
-                        <p><?php echo $propertie['parking']; ?></p>
-                    </li> 
-                    <li>
-                        <img class="dark-icon" loading="lazy" src="build/img/icono_dormitorio.svg" alt="room icon">
-                        <p><?php echo $propertie['bedrooms']; ?></p>
-                    </li>  
-                    <li>
-                        <img class="dark-icon" loading="lazy" src="build/img/icono_wc.svg" alt="wc icon">
-                        <p><?php echo $propertie['wc']; ?></p>
-                    </li>                           
-                </ul>
+                    <ul class="feature-icons">    
+                        <li>
+                            <img class="dark-icon" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="parking icon">
+                            <p><?php echo $propertie['parking']; ?></p>
+                        </li> 
+                        <li>
+                            <img class="dark-icon" loading="lazy" src="build/img/icono_dormitorio.svg" alt="room icon">
+                            <p><?php echo $propertie['bedrooms']; ?></p>
+                        </li>  
+                        <li>
+                            <img class="dark-icon" loading="lazy" src="build/img/icono_wc.svg" alt="wc icon">
+                            <p><?php echo $propertie['wc']; ?></p>
+                        </li>                           
+                    </ul>
 
-                <a href="announcement.php?id=<?php echo $propertie['id']; ?>" 
-                   class="yellow-button-block"
-                   data-translate="anuncio-boton">
-                   Explore Property
-                </a>
-
+                    <a href="announcement.php?id=<?php echo $propertie['id']; ?>" 
+                    class="yellow-button-block"
+                    data-translate="anuncio-boton">
+                    Explore Property
+                    </a>
+                </div>
             </div> </div> <?php endwhile; ?>
 </div>
 
